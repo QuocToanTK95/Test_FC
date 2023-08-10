bf4c0558cc64a5b433f8adf29bfe0783808ae795
QuocToanTK95 <quoctoantk95@gmail.com>
2023-08-10 12:51:10 +0700

    add dueDate and Modal

--- main.js
+++ main.js
@@ -1,41 +1,81 @@
 $(document).ready(function () {
   const newTaskInput = $("#new-task");
+  const dueDate = $("#dueDate");
   const taskList = $("#task-list");
 
   function addTask() {
-    const taskText = newTaskInput.val();
-    if (taskText.trim() !== "") {
-      const taskItem = $(
-        '<li><span class="task-text">' +
-          taskText +
-          '</span><button class="edit-button">Edit</button><button class="delete-button">Delete</button></li>'
-      );
-
-      //   Trigger edit button
-      taskItem.find(".edit-button").click(function () {
-        const editText = $(this).siblings(".task-text");
-        const newText = prompt("Edit task:", editText.text());
-        if (newText !== null && newText.trim() !== "") {
-          editText.text(newText);
-          saveTasksToLocalStorage();
-        }
-      });
-
-      //   Trigger delete button
-      taskItem.find(".delete-button").click(function () {
-        const taskText = $(this).siblings(".task-text").text();
-        const shouldDelete = confirm(
-          `Are you sure you want to delete task "${taskText}"?`
+    if (newTaskInput.val() === "" || dueDate.val() === "") {
+      alert("Please enter a task and a due date.");
+    } else {
+      const taskText = newTaskInput.val();
+      if (taskText.trim() !== "") {
+        const taskItem = $(
+          '<li><span class="task-text">' +
+            taskText +
+            " - Due: " +
+            dueDate.val() +
+            '</span><button class="edit-button">Edit</button><button class="delete-button">Delete</button></li>'
         );
-        if (shouldDelete) {
-          $(this).parent().remove();
-          saveTasksToLocalStorage();
-        }
-      });
 
-      taskList.append(taskItem);
-      newTaskInput.val("");
-      saveTasksToLocalStorage();
+        // Trigger edit button
+        taskItem.find(".edit-button").click(function () {
+          const editText = $(this).siblings(".task-text").text();
+          const editModal = $("#editModal");
+          const editTaskInput = $("#editTaskInput");
+
+          editTaskInput.val(editText);
+          editModal.show();
+
+          $("#confirmEdit")
+            .off("click")
+            .click(function () {
+              const newText = editTaskInput.val();
+              if (newText.trim() !== "") {
+                const taskTextElement = taskItem.find(".task-text");
+                taskTextElement.text(newText);
+                editModal.hide();
+                saveTasksToLocalStorage();
+              }
+            });
+
+          $("#cancelEdit")
+            .off("click")
+            .click(function () {
+              editModal.hide();
+            });
+        });
+
+        // Trigger delete button
+        taskItem.find(".delete-button").click(function () {
+          const deleteItem = $(this).closest("li"); // Get the parent <li> element
+          const taskText = deleteItem.find(".task-text").text();
+          const deleteModal = $("#deleteModal");
+
+          deleteModal
+            .find(".modal-content p")
+            .text(`Are you sure you want to delete task "${taskText}"?`);
+          deleteModal.show();
+
+          $("#confirmDelete")
+            .off("click")
+            .click(function () {
+              deleteModal.hide();
+              deleteItem.remove(); // Remove the task item
+              saveTasksToLocalStorage();
+            });
+
+          $("#cancelDelete")
+            .off("click")
+            .click(function () {
+              deleteModal.hide();
+            });
+        });
+
+        taskList.append(taskItem);
+        newTaskInput.val("");
+        dueDate.val("");
+        saveTasksToLocalStorage();
+      }
     }
   }
 
